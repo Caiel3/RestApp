3e85482f6a6f42803017198f32ac4e2f8f3058b6
DIDIER ALEJANDRO MARTINEZ MOLINA <didiermartinez225450@correo.itm.edu.co>
2020-11-02 14:46:10 -0500

    Add structure

---
+++ RestApp.Common/RestApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RestApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
