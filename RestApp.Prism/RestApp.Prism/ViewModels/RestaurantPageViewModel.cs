ab818f3188c4924214a676cc4c984b5aba39a518
DIDIER ALEJANDRO MARTINEZ MOLINA <didiermartinez225450@correo.itm.edu.co>
2020-11-12 18:29:38 -0500

    Add restaurant name to poin of sale

--- RestApp.Prism/RestApp.Prism/ViewModels/RestaurantPageViewModel.cs
+++ RestApp.Prism/RestApp.Prism/ViewModels/RestaurantPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Navigation;
+﻿using Prism.Commands;
+using Prism.Navigation;
 using RestApp.Common.Entities;
 using RestApp.Common.Responses;
 using RestApp.Common.Services;
@@ -6,6 +7,7 @@ using RestApp.Prism.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using Xamarin.Essentials;
 
@@ -17,13 +19,34 @@ namespace RestApp.Prism.ViewModels
         private readonly INavigationService _navigationService;
         private readonly IApiService _apiService;
         private ObservableCollection<PointSale> _pointsale;
+        private bool _isRunning;
+        private string _search;
+        private List<PointSale> _myRestaurants;
+        private DelegateCommand _searchCommand;
+
 
         public RestaurantPageViewModel(INavigationService navigationService, IApiService apiService) : base(navigationService)
         {
             _navigationService = navigationService;
             _apiService = apiService;
             Title = Languages.Restaurants;
-            LoadPointsSalesAsync();
+            LoadPointsSalesAsync();           
+        }
+        public DelegateCommand SearchCommand => _searchCommand ?? (_searchCommand = new DelegateCommand(ShowRestaurants));
+        public string Search
+        {
+            get => _search;
+            set
+            {
+                SetProperty(ref _search, value);
+                ShowRestaurants();
+            }
+        }     
+
+        public bool IsRunning
+        {
+            get => _isRunning;
+            set => SetProperty(ref _isRunning, value);
         }
 
         public ObservableCollection<PointSale> PointSale
@@ -39,12 +62,17 @@ namespace RestApp.Prism.ViewModels
                 await App.Current.MainPage.DisplayAlert(Languages.Error, Languages.ConnectionError, Languages.Accept);
                 return;
             }
+            IsRunning = true;
 
             string url = App.Current.Resources["UrlAPI"].ToString();
             Response response = await _apiService.GetListAsync<PointSale>(
                 url,
                 "/api",
                 "/PointsSale");
+
+            IsRunning = false;
+            _myRestaurants = (List<PointSale>)response.Result;
+            ShowRestaurants();
 
             if (!response.IsSuccess)
             {
@@ -57,6 +85,18 @@ namespace RestApp.Prism.ViewModels
 
             List<PointSale> myPointSales = (List<PointSale>)response.Result;
             PointSale = new ObservableCollection<PointSale>(myPointSales);
+        }
+        private void ShowRestaurants()
+        {
+            if (string.IsNullOrEmpty(Search))
+            {
+                PointSale = new ObservableCollection<PointSale>(_myRestaurants);
+            }
+            else
+            {
+                PointSale = new ObservableCollection<PointSale>(_myRestaurants
+                    .Where(p => p.Name.ToLower().Contains(Search.ToLower())));
+            }
         }
     }
 
