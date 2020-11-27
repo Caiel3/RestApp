a82bc7b22957dd2de23cdef742f2ada23b51d5f7
DIDIER ALEJANDRO MARTINEZ MOLINA <didiermartinez225450@correo.itm.edu.co>
2020-11-26 20:58:49 -0500

    Maps part 2

--- RestApp.Prism/RestApp.Prism/Views/RestaurantLocationPage.xaml.cs
+++ RestApp.Prism/RestApp.Prism/Views/RestaurantLocationPage.xaml.cs
@@ -1,12 +1,13 @@
 ﻿using Plugin.Permissions;
 using Plugin.Permissions.Abstractions;
+using RestApp.Common.Responses;
 using RestApp.Common.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
@@ -17,16 +18,46 @@ namespace RestApp.Prism.Views
     public partial class RestaurantLocationPage : ContentPage
     {
         private readonly IGeolocatorService _geolocatorService;
+        private readonly IApiService _apiService;
 
-        public RestaurantLocationPage(IGeolocatorService geolocatorService)
+        public RestaurantLocationPage(IGeolocatorService geolocatorService,IApiService apiService)
         {
             InitializeComponent();
             _geolocatorService = geolocatorService;
+            _apiService = apiService;
         }
         protected override void OnAppearing()
         {
             base.OnAppearing();
             MoveMapToCurrentPositionAsync();
+            LoadPointSalesAsync();
+        }
+        private async void LoadPointSalesAsync()
+        {
+            if (Connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                return;
+            }
+
+            string url = App.Current.Resources["UrlAPI"].ToString();
+            Response response = await _apiService.GetListAsync<PointSaleResponse>(url, "api", "/PointsSale");
+
+            if (!response.IsSuccess)
+            {
+                return;
+            }
+
+            List<PointSaleResponse> pointsales = (List<PointSaleResponse>)response.Result;
+            foreach (PointSaleResponse pointsale in pointsales)
+            {
+                MyMap.Pins.Add(new Pin
+                {
+                    Address = pointsale.Address,
+                    Label = pointsale.Restaurant.Name+" "+pointsale.Name,
+                    Position = new Position(pointsale.Latitude, pointsale.Longitude),
+                    Type = PinType.Place
+                });
+            }
         }
 
         private async void MoveMapToCurrentPositionAsync()
@@ -52,12 +83,12 @@ namespace RestApp.Prism.Views
 
         private async Task<bool> CheckLocationPermisionsAsync()
         {
-            PermissionStatus permissionLocation = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Location);
-            PermissionStatus permissionLocationAlways = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.LocationAlways);
-            PermissionStatus permissionLocationWhenInUse = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.LocationWhenInUse);
-            bool isLocationEnabled = permissionLocation == PermissionStatus.Granted ||
-                                        permissionLocationAlways == PermissionStatus.Granted ||
-                                        permissionLocationWhenInUse == PermissionStatus.Granted;
+            Plugin.Permissions.Abstractions.PermissionStatus permissionLocation = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Location);
+            Plugin.Permissions.Abstractions.PermissionStatus permissionLocationAlways = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.LocationAlways);
+            Plugin.Permissions.Abstractions.PermissionStatus permissionLocationWhenInUse = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.LocationWhenInUse);
+            bool isLocationEnabled = permissionLocation == Plugin.Permissions.Abstractions.PermissionStatus.Granted ||
+                                        permissionLocationAlways == Plugin.Permissions.Abstractions.PermissionStatus.Granted ||
+                                        permissionLocationWhenInUse == Plugin.Permissions.Abstractions.PermissionStatus.Granted;
             if (isLocationEnabled)
             {
                 return true;
@@ -68,9 +99,10 @@ namespace RestApp.Prism.Views
             permissionLocation = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Location);
             permissionLocationAlways = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.LocationAlways);
             permissionLocationWhenInUse = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.LocationWhenInUse);
-            return permissionLocation == PermissionStatus.Granted ||
-                    permissionLocationAlways == PermissionStatus.Granted ||
-                    permissionLocationWhenInUse == PermissionStatus.Granted;
+            return permissionLocation == Plugin.Permissions.Abstractions.PermissionStatus.Granted ||
+                    permissionLocationAlways == Plugin.Permissions.Abstractions.PermissionStatus.Granted ||
+                    permissionLocationWhenInUse == Plugin.Permissions.Abstractions.PermissionStatus.Granted;
         }
+
     }
 }
